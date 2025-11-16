46b61da1398149282b49944d4cdce80231799bef
injure <kiriambus@mail.ru>
2025-11-17 04:29:04 +1000

    Refactoring & Update backend

--- src/bot.py
+++ src/bot.py
@@ -3,8 +3,10 @@ import json
 from dotenv import load_dotenv
 import os
 from model import pdf2json
+import re
 
 load_dotenv()
+
 
 # Получаем токен бота из переменной среды
 BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
@@ -21,11 +23,13 @@ def send_welcome(message):
 @bot.message_handler(content_types=['document', 'photo'])
 def handle_document(message):
     # Проверяем, что загруженный файл - PDF
-    if message.document.mime_type == 'application/pdf':
+    if message.content_type != 'photo' and message.document.mime_type == 'application/pdf':
+        print(f'Получен файл для обработки в чате {message.chat.id} (№{message.message_id})')
+        bot_message = bot.reply_to(message, f'Файл отправлен в обработку, пожалуйста, подождите...', parse_mode='Markdown')
         file_info = bot.get_file(message.document.file_id)
         downloaded_file = bot.download_file(file_info.file_path)
 
-        # Сохраняем файл на сервере (или обрабатываем в памяти)
+        # Сохраняем файл на сервере
         file_name = f"temp_{message.message_id}.pdf"
         with open(file_name, 'wb') as new_file:
             new_file.write(downloaded_file)
@@ -39,8 +43,33 @@ def handle_document(message):
         # Вывод полученного JSON для отладки
         print(output)
 
+        if "NOT_RECEIPT" in output:
+            bot.edit_message_text('Отправленный файл не был определен как чек.', bot_message.chat.id, bot_message.message_id)
+            return
+
+        if "ERROR" in output[0]:
+            bot.edit_message_text('При обработке файла произошла ошибка', bot_message.chat.id, bot_message.message_id)
+            print(f'ОШИБКА: При обработке файла в чате {message.chat.id} (№{message.message_id}) произошла ошибка: {output[1]}')
+            return
+
+        # Получение оригинального названия файла
+        output_name = f'output_{message.message_id}.json'
+
+        # Сохраняем вывод на сервере
+        with open(f'{output_name}', 'w') as new_file:
+            new_file.write(output)
+
         # Отправляем результат пользователю
-        bot.reply_to(message, f'{output}', parse_mode='Markdown')
+        try:
+            with open(f'{output_name}', 'rb') as f:
+                bot.send_document(message.chat.id, f, caption="Запрос обработан успешно", reply_to_message_id=message.message_id, parse_mode='Markdown')
+                print(f"Файл в чате {message.chat.id} (№{message.message_id}) обработан успешно")
+        except Exception as e:
+            bot.edit_message_text('Произошла ошибка отправки результата.', bot_message.chat.id, bot_message.message_id)
+            print(f"ОШИБКА: При отправке файла в чате {message.chat.id} (№{message.message_id}) произошла ошибка: {e}")
+        else:
+            bot.delete_message(bot_message.chat.id, bot_message.message_id)
+            os.remove(output_name)
 
     else:
         bot.reply_to(message, "Пожалуйста, отправьте файл в формате PDF.")
